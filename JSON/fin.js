dc4b2ffa3f96309e233f91f2569960cce3675bcf
nitishkumar-13 <nitishkumars1302.com>
2024-02-23 13:51:51 +0530

    taskday5

---
+++ JSON/fin.js
@@ -0,0 +1,20 @@
+// 1) Using for...in loop
+for (let key in rd) {
+  console.log(key, ":", rd[key]);
+}
+
+// 2) Using for loop
+for (let i = 0; i < rd.education.length; i++) {
+  console.log("education:", rd.education[i]);
+}
+
+//  4) Using for...of loop (for arrays)
+for (let education of rd.education) {
+  console.log("degree:", education.degree);
+  console.log("yearOfPassing:", education.yearOfPassing);
+}
+
+// 5) Using forEach loop (for arrays)
+rd.education.forEach((edu) => {
+  console.log("Degree:", edu.degree);
+});
---
+++ Readme.txt
@@ -0,0 +1,8 @@
+Task5:  
+
+1) Create your own Resume data in JSON format
+2) For the  above, iterate over all for loops(for, for in, for of, forEach)
+
+I am Learning the new things
+
+Thank you so much Madam/Sir
